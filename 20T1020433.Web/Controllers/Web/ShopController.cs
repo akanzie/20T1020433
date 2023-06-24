3ce6bd023d31bfd5e4ba15b2b0fc206cf4ad3704
Kiet Chau <anhkiet029@gmail.com>
2023-06-24 15:09:39 +0700

    Detail

--- 20T1020433.Web/Controllers/Web/ShopController.cs
+++ 20T1020433.Web/Controllers/Web/ShopController.cs
@@ -1,4 +1,5 @@
 ﻿using _20T1020433.BusinessLayers;
+using _20T1020433.DomainModels;
 using _20T1020433.Web.Models;
 using System;
 using System.Collections.Generic;
@@ -12,6 +13,7 @@ namespace _20T1020433.Web.Controllers.Web
     {
         private const int PAGE_SIZE = 10;
         private const string PRODUCT_SEARCH = "SearchProductCondition";
+        private const string SHOPPING_CART = "ShoppingCart";
         private const string SUCCESS_MESSAGE = "SuccessMessage";
         private const string ERROR_MESSAGE = "ErrorMessage";
         // GET: Home
@@ -57,6 +59,75 @@ namespace _20T1020433.Web.Controllers.Web
             };
             Session[PRODUCT_SEARCH] = condition;
             return View(result);
+        }
+        private List<OrderDetail> GetShoppingCart()
+        {
+            List<OrderDetail> shoppingCart = Session[SHOPPING_CART] as List<OrderDetail>;
+            if (shoppingCart == null)
+            {
+                shoppingCart = new List<OrderDetail>();
+                Session[SHOPPING_CART] = shoppingCart;
+            }
+            return shoppingCart;
+        }
+
+        public ActionResult Detail(int id = 0)
+        {
+            if (id <= 0)
+                return RedirectToAction("Index");
+            var product = ProductDataService.GetProduct(id);
+            if (product == null)
+                return RedirectToAction("Index");
+            var data = new ProductModel()
+            {
+                ProductID = product.ProductID,
+                ProductName = product.ProductName,
+                CategoryID = product.CategoryID,
+                SupplierID = product.SupplierID,
+                Unit = product.Unit,
+                Price = product.Price,
+                Photo = product.Photo,
+                Attributes = ProductDataService.ListAttributes(id),
+                Photos = ProductDataService.ListPhotos(id)
+            };
+            if (data == null)
+                return RedirectToAction("Index");
+            return View(data);            
+        }
+        public ActionResult Create()
+        {
+            ViewBag.ErrorMessage = TempData[ERROR_MESSAGE] ?? "";
+            return View(GetShoppingCart());
+        }
+        [HttpPost]
+        public ActionResult AddToCart(OrderDetail data)
+        {
+            if (data == null)
+            {
+                TempData[ERROR_MESSAGE] = "Dữ liệu không hợp lệ";
+                return RedirectToAction("Create");
+            }
+            if (data.SalePrice <= 0 || data.Quantity <= 0)
+            {
+                TempData[ERROR_MESSAGE] = "Giá bán và số lượng không hợp lệ";
+                return RedirectToAction("Create");
+            }
+
+            List<OrderDetail> shoppingCart = GetShoppingCart();
+            var existsProduct = shoppingCart.FirstOrDefault(m => m.ProductID == data.ProductID);
+
+            if (existsProduct == null) //Nếu mặt hàng cần được bổ sung chưa có trong giỏ hàng thì bổ sung vào giỏ
+            {
+
+                shoppingCart.Add(data);
+            }
+            else //Trường hợp mặt hàng cần bổ sung đã có thì tăng số lượng và thay đổi đơn giá
+            {
+                existsProduct.Quantity += data.Quantity;
+                existsProduct.SalePrice = data.SalePrice;
+            }
+            Session[SHOPPING_CART] = shoppingCart;
+            return RedirectToAction("Create");
         }
     }
 }
