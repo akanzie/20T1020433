34d515fb7c81ef54e0ec66910527c4bb2d874bae
akanzie <114471183+akanzie@users.noreply.github.com>
2023-07-08 19:55:12 +0700

    sửa giao diện shop

---
+++ 20T1020433.Web/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _20T1020433.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Input()
+        {
+            return View();
+        }
+    }
+}
